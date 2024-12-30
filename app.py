9f3c2cc4d6d32f5087058805a59ac76b0a258242
Rinshad Choorappara <101931172+tinkvu@users.noreply.github.com>
2024-12-30 23:50:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,7 @@ If you are sure that your english is perfect and you can understand any Irish ac
 # Embed the YouTube video using iframe
 st.components.v1.html(
     """
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/nJ7QB3om-QY?si=MnoNtWFO8J7sJ2NU" 
-    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-    gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-    </iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/nJ7QB3om-QY?si=MnoNtWFO8J7sJ2NU&amp;start=27" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     """,
     height=315,
 )
