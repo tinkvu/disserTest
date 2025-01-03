df1d5c58137279b1a39b6d22977bf502fc9dde43
Rinshad Choorappara <101931172+tinkvu@users.noreply.github.com>
2025-01-03 23:17:04 +0000

    changed to llama-3.3-70b-versatile

--- pages/appfunctions.py
+++ pages/appfunctions.py
@@ -84,7 +84,7 @@ if "welcome_shown" not in st.session_state and "user_details" in st.session_stat
 def translate_text(text, target_language):
     try:
         response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {"role": "system", "content": "Translate the following text into " + target_language + ". Response should be just only the translation. Example input: Irish Slang; Example response: Argot irlandés"},
                 {"role": "user", "content": text},
@@ -284,7 +284,7 @@ def generate_response(text, target_language):
         # start_time = time.time()
         
         completion = client.chat.completions.create(
-            model="llama-3.2-1b-preview",
+            model="llama-3.3-70b-versatile",
             messages=api_messages,
             temperature=1,
             max_tokens=1024,
